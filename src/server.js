b8d69e0feb0114a1d868763acc328cdb3014606e
Tanapon <64050470@kmitl.ac.th>
2025-04-02 12:29:08 +0700

    start server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Test 470");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+
+// console.log("Hello uu")
