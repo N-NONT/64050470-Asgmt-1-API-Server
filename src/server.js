81ea815ba3f2a2dd750d8fb8bf8450952e3af6b8
TANAPON PANSOOK <64050470@kmitl.ac.th>
2025-04-02 12:46:42 +0700

    structure server

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Test 470");
-});
+app.use(cors());
+app.use(express.json());
+
+
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
-
-
-
-// console.log("Hello uu")
